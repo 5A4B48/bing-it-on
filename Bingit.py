e1d3141899b9e2a326d9d65d1923fc84f5e9eb80
Zachary Hunsaker <hunsaker@ucar.edu>
2019-04-11 11:24:31 -0600

    fixed formatting

--- Bingit.py
+++ Bingit.py
@@ -6,13 +6,15 @@ import random
 
 terms = []
 
+
 def getterms():
 	a = requests.get('https://corporatebs-generator.sameerkumar.website/')
 	b = json.loads(a.content.decode('latin-1'))
 	c = b.get('phrase').split()
 	return c
-	
-while len(terms)<31:
+
+		
+while len(terms) < 31:
 	d = getterms()
 	for i in d:
 		terms.append(i)
@@ -20,6 +22,6 @@ while len(terms)<31:
 
 for q in terms:
 	webbrowser.open("https://bing.com/search?q=" + q)
-	time.sleep(random.randint(1,3))
+	time.sleep(random.randint(1, 3))
 
 
